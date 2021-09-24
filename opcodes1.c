378b3e56a7aed8b0cb2c55599e16f0d0a65fa6ed
Leul Yiheyis <leulykpro@gmail.com>
2021-09-24 05:25:27 +0300

    check if both the pointer and the node are null

--- opcodes1.c
+++ opcodes1.c
@@ -73,7 +73,7 @@ void pop(stack_t **stack, unsigned int line_number)
 {
 	stack_t *temp = NULL;
 
-	if (*stack == NULL)
+	if (!stack && !(*stack))
 	{
 		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
 		_exit(EXIT_FAILURE);
