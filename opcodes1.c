a6e4e9691a3d7440ba81ee78ebef88741dfa26a5
Leul Yiheyis <leulykpro@gmail.com>
2021-09-24 05:30:40 +0300

    check node not address

--- opcodes1.c
+++ opcodes1.c
@@ -34,7 +34,7 @@ void push(stack_t **stack, unsigned int line_number)
  */
 void pint(stack_t **stack, unsigned int line_number)
 {
-	if (stack == NULL)
+	if (*stack == NULL)
 	{
 		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
 		_exit(EXIT_FAILURE);
