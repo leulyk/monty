f295f89db6603ef8235023e328a45fc453289f57
Leul Yiheyis <leulykpro@gmail.com>
2021-09-24 05:27:24 +0300

    revert back|

--- opcodes1.c
+++ opcodes1.c
@@ -73,7 +73,7 @@ void pop(stack_t **stack, unsigned int line_number)
 {
 	stack_t *temp = NULL;
 
-	if (!stack && !(*stack))
+	if (*stack == NULL)
 	{
 		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
 		_exit(EXIT_FAILURE);
